4cd974875b7e111933a5e982a048d70eecedadd5
yellakrishna <yellakrishna7675@gmail.com>
2025-01-01 18:10:01 +0530

    color

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -38,13 +38,13 @@ const Cart = () => {
           <div>
             <div className="cart-total-details"><p>Subtotal</p><p>${getTotalCartAmount()}</p></div>
             <hr />
-            <div className="cart-total-details"><p>Delivery Fee</p><p>${getTotalCartAmount()===0?0:5}</p></div>
+            <div className="cart-total-details"><p>Delivery Fee</p><p>${getTotalCartAmount()===0?0:30}</p></div>
             <hr />
             <div className="cart-total-details"><b>Total</b><b>${getTotalCartAmount()===0?0:getTotalCartAmount()+5}</b></div>
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
-        <div className="cart-promocode">
+        {/* <div className="cart-promocode">
           <div>
             <p>If you have a promo code, Enter it here</p>
             <div className='cart-promocode-input'>
@@ -52,7 +52,7 @@ const Cart = () => {
               <button>Submit</button>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
