ca4241dc0769ac2ce16b821730336bbeb2e2d645
yellakrihsna <115071795+yellakrishna@users.noreply.github.com>
2025-01-02 06:34:27 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Fish Online Servies <strong> Alampur Mondal</strong></h2>
+                <h2>Fish Online Servies <strong> Alampur Mandal</strong></h2>
                 {/* <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p> */}
                 <Link to = "/menu" ><button className='view-button'>View Menu</button></Link>
             </div>
